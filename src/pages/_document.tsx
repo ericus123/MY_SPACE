2bcad11b92a21a1e82dbfec4e29f37df77c760fd
AMANI Eric <amaniericus@gmail.com>
2023-07-23 11:55:13 +0200

    ft(adsense): implement adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="keywords"
+          content="CSS, C, SCSS, Next,TypeScript, Electronics, HTML, JavaScript, Node, React,Arduino, Security, Nginx,Database"
+        />
+        <meta name="author" content="AMANI Eric" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
